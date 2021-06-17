a4dfaff9d4a7f81f6253f366a170a320829ed9f4
yoosefi <code@yoosefi.info>
2021-06-17 04:37:17 -0700

    DateTimeTrait :: added limited timezone conversion functions

--- src/DB/SQL/DateTimeTrait.php
+++ src/DB/SQL/DateTimeTrait.php
@@ -143,6 +143,50 @@ trait DateTimeTrait {
     }
 
     /**
+     * Changes the timezone from UTC to the local timezone.
+     *
+     * - SQLite: Uses the operating system's timezone.
+     * - MySQL: Uses PHP's timezone.
+     *
+     * > Warning: Chaining this multiple times will further change the timezone offset.
+     *
+     * @return DateTime
+     */
+    public function toLocalTz () {
+        if ($this->db->isSQLite()) {
+            // docs:
+            // > The "localtime" modifier (12) assumes the time value to its left is in
+            // > Universal Coordinated Time (UTC) and adjusts that time value so that it is in localtime.
+            // > If "localtime" follows a time that is not UTC, then the behavior is undefined.
+            return DateTime::factory($this->db, "DATETIME({$this},'localtime')");
+        }
+        $local = date_default_timezone_get();
+        return DateTime::factory($this->db, "CONVERT_TZ({$this},'UTC','{$local}')");
+    }
+
+    /**
+     * Changes the timezone from the local timezone to UTC.
+     *
+     * > Warning: Datetimes are already stored and retrieved as UTC.
+     * > Only use this if you know the expression is in the local timezone.
+     *
+     * > Warning: Chaining this multiple times will further change the timezone offset.
+     *
+     * @return DateTime
+     */
+    public function toUtc () {
+        if ($this->db->isSQLite()) {
+            // docs:
+            // > "utc" assumes that the time value to its left is in the local timezone
+            // > and adjusts that time value to be in UTC. If the time to the left is not in localtime,
+            // > then the result of "utc" is undefined.
+            return DateTime::factory($this->db, "DATETIME({$this},'utc')");
+        }
+        $local = date_default_timezone_get();
+        return DateTime::factory($this->db, "CONVERT_TZ({$this},'{$local}','UTC')");
+    }
+
+    /**
      * `00` to `53`
      *
      * @return Num
