222f48582400bfb1c355bb3157d30a6d6354aaa6
yoosefi <code@yoosefi.info>
2021-06-17 21:46:30 -0700

    TextTrait :: moved toDateTime() to the Text class

--- src/DB/SQL/Text.php
+++ src/DB/SQL/Text.php
@@ -8,4 +8,23 @@ namespace Helix\DB\SQL;
 class Text extends Value {
 
     use TextTrait;
+
+    /**
+     * Interpret the expression as a datetime.
+     *
+     * > Warning: If the expression is in the local timezone
+     * > you should chain this with {@link DateTime::toUtc()}
+     *
+     * SQLite:
+     * - The expression's value must conform to one of any `time-value` formats.
+     * - https://www.sqlite.org/lang_datefunc.html
+     *
+     * MySQL:
+     * - The expression's value must conform to `YYYY-MM-DD hh:mm:ss`
+     *
+     * @return DateTime
+     */
+    public function toDateTime () {
+        return DateTime::factory($this->db, $this);
+    }
 }
